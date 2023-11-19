1ab422e4d3f593f38a51c851f163c4059a8ace0a
Santiago Armstrong <santiago.armstrong@cencosud.cl>
2023-11-19 17:52:24 -0300

    ADD: chile parser

---
+++ cpilatam/parsers/chile.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""This module contains a parser for the Chile CPI data."""
+
+from cpilatam.parsers.base import BaseCPIParser
+
+
+class ChileCPIParser(BaseCPIParser):
+    pass
