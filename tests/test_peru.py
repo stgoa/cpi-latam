7525c340277558a244326f9eba3ac02707976040
pablo.torresgutierrez <pablo.torresgutierrez@cencosud.cl>
2023-11-28 17:00:17 -0300

    UPDATE: Update test

--- tests/test_peru.py
+++ tests/test_peru.py
@@ -9,18 +9,22 @@ class TestPeruParser:
     @pytest.fixture
     def setUp(self, monkeypatch):
         # crear instancia
-        self.parser = PeruCPIParser()
+        self.parser = PeruCPIParser(local_file_path="data/raw/peru.csv")
 
         # mock download using monkeypatch
         monkeypatch.setattr(self.parser, "download", self.mock_download)
 
     def mock_download(self):
         # Read the data and assign it to self.data
-        self.parser.data = pd.read_csv("data/raw/peru.csv")
+        # The CSV has unrecognized Symbols. Another alternative is to replace those symbols "(?)" to "Í"
+        self.parser.data = pd.read_csv("data/raw/peru.csv", encoding="latin-1")
 
     def test_parse(self, setUp):
         # read data
         self.parser.download()
+
+        # proceed to parse the data
+        self.parser.parse()
 
         # assert that the schema is correct
         CPI_SCHEMA.validate(self.parser.data)
