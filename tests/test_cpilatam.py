82e7fbb37fe1605e560eed3f302ebff5adad103e
Santiago Armstrong <santiago.armstrong@cencosud.cl>
2023-11-28 20:58:01 -0300

    ADD: test for main functionality

---
+++ tests/test_cpilatam.py
@@ -0,0 +1,11 @@
+def test_import():
+    import cpilatam  # noqa: F401
+
+
+def test_dataframe_schemas():
+    from cpilatam import DF_CPI
+    from cpilatam.schemas import CPI_SCHEMA
+
+    assert isinstance(DF_CPI, dict)
+    for df in DF_CPI.values():
+        CPI_SCHEMA.validate(df)
