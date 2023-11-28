5f441c57afeb669c5fe9483214415068e11fe202
pablo.torresgutierrez <pablo.torresgutierrez@cencosud.cl>
2023-11-28 18:46:49 -0300

    UPDATE: Edit clean location of the file

--- cpilatam/settings.py
+++ cpilatam/settings.py
@@ -89,10 +89,10 @@ class Settings(BaseSettings):
     LOG_DESTINATION: LogDest = LogDest.CONSOLE.value
     """Destination for logs."""
 
-    COLOMBIA_LOCAL_PATH: Path = Path(PACKAGE_PATH.parent, "data", "colombia_cpi.xlsx")
+    COLOMBIA_LOCAL_PATH: Path = Path(PACKAGE_PATH.parent, "data", "clean", "colombia.csv")
     """Path to local file with Colombia CPI data."""
 
-    PERU_LOCAL_PATH: Path = Path(PACKAGE_PATH.parent, "data", "peru_cpi.csv")
+    PERU_LOCAL_PATH: Path = Path(PACKAGE_PATH.parent, "data", "clean", "peru.csv")
     """Path to local file with Peru CPI data."""
 
     class Config:
